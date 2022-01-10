270966b34e0cb993bb63256581efbbe242a09212
Alexander Fedorov <fedorov7@gmail.com>
2022-01-10 07:51:49 +0300

    feat(lsp): fix deprecated commands

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -128,33 +128,33 @@ local mappings = {
     name = "LSP",
     a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
     d = {
-      "<cmd>Telescope lsp_document_diagnostics<cr>",
+      "<cmd>Telescope diagnostics bufnr=0<cr>",
       "Document Diagnostics",
     },
     w = {
-      "<cmd>Telescope lsp_workspace_diagnostics<cr>",
+      "<cmd>Telescope diagnostics<cr>",
       "Workspace Diagnostics",
     },
     f = { "<cmd>lua vim.lsp.buf.formatting()<cr>", "Format" },
     i = { "<cmd>LspInfo<cr>", "Info" },
     I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
     j = {
-      "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>",
+      "<cmd>lua vim.diagnostic.goto_next()<CR>",
       "Next Diagnostic",
     },
     k = {
-      "<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>",
+      "<cmd>lua vim.diagnostic.goto_prev()<cr>",
       "Prev Diagnostic",
     },
     l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
-    q = { "<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>", "Quickfix" },
+    q = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix" },
     r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
     s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
     S = {
       "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
       "Workspace Symbols",
     },
-    F = { "<cmd>lua vim.diagnostic.open_float()<CR>", "Open float" },
+    F = { "<cmd>lua vim.diagnostic.open_float()<CR>", "Open float diagnostic" },
     h = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "SignatureHelp" },
   },
   s = {
@@ -202,11 +202,11 @@ local short_opts = {
 local short_mappings = {
   gD = { "<cmd>lua vim.lsp.buf.declaration()<CR>", "Go to declaration" },
   gd = { "<cmd>lua vim.lsp.buf.definition()<CR>", "Go to definition" },
-  gK = { "<cmd>lua vim.lsp.buf.hover()<CR>", "Hover" },
   gi = { "<cmd>lua vim.lsp.buf.implementation()<CR>", "Go to implementation" },
   gr = { "<cmd>lua vim.lsp.buf.references()<CR>", "Go to reference" },
+  gK = { "<cmd>lua vim.lsp.buf.hover()<CR>", "Hover" },
   gR = { "<cmd>Trouble lsp_references<CR>", "Trouble LSP Reference" },
-  gl = { '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ border = "rounded" })<CR>', "Line diagnostics" },
+  gl = { "<cmd>lua vim.diagnostic.open_float()<CR>", "Line diagnostics" },
 }
 
 which_key.setup(setup)
