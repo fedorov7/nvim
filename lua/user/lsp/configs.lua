055d2593bde2d5ac4614caf4c7e14cd9b8ca5f13
Alexander Fedorov <fedorov7@gmail.com>
2022-07-21 15:13:58 +0300

    feat(lsp): add pyrigth server setup

--- lua/user/lsp/configs.lua
+++ lua/user/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "jsonls", "sumneko_lua", "clangd", "cmake" }
+local servers = { "jsonls", "sumneko_lua", "clangd", "cmake", "pyright" }
 
 lsp_installer.setup({
   ensure_installed = servers,
