041931366651b60a1ddb68ebfad7d82a46c389bb
Alexander Fedorov <fedorov7@gmail.com>
2022-08-23 11:25:23 +0300

    feat(plugins): disable copilot

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -94,7 +94,7 @@ return packer.startup(function(use)
   use("b0o/SchemaStore.nvim")
 
   -- Copilot
-  use("github/copilot.vim")
+  -- use("github/copilot.vim")
 
   use({
     "folke/trouble.nvim",
